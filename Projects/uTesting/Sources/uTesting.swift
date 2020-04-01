04ec42c6314769fa55987f8c0a2f536aacec539c
Pedro Piñera <pedro@ppinera.es>
2020-04-01 14:53:40 +0200

    Add uTesting as a dependency

---
+++ Projects/uTesting/Sources/uTesting.swift
@@ -0,0 +1,6 @@
+import Foundation
+import XCTest
+
+public extension XCTest {
+    
+}
--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -1,15 +1,20 @@
 import ProjectDescription
 
+public enum uFeatureTarget {
+    case framework
+    case tests
+    case examples
+    case testing
+}
+
+
 extension Project {
     
-    public static func app(name: String, platform: Platform, dependencies: [TargetDependency] = []) -> Project {
-        return self.project(name: name, product: .app, platform: platform, dependencies: dependencies, infoPlist: [
-            "CFBundleShortVersionString": "1.0",
-            "CFBundleVersion": "1"
-        ])
-    }
     
-    public static func framework(name: String, dependencies: [TargetDependency] = []) -> Project {
+    public static func framework(name: String,
+                                 targets: Set<uFeatureTarget> = Set([.framework, .tests, .examples, .testing]),
+                                 dependencies: [String] = [],
+                                 sdks: [String] = []) -> Project {
         let frameworkConfigurations: [CustomConfiguration] = [
             .debug(name: "Debug", settings: [String: SettingValue](), xcconfig: .relativeToRoot("Configurations/iOS/iOS-Framework.xcconfig")),
             .debug(name: "Release", settings: [String: SettingValue](), xcconfig: .relativeToRoot("Configurations/iOS/iOS-Framework.xcconfig"))
@@ -26,71 +31,64 @@ extension Project {
             .debug(name: "Debug", settings: [String: SettingValue](), xcconfig: .relativeToRoot("Configurations/Base/Configurations/Debug.xcconfig")),
             .debug(name: "Release", settings: [String: SettingValue](), xcconfig: .relativeToRoot("Configurations/Base/Configurations/Release.xcconfig"))
         ]
+        
+        var testsDependencies: [TargetDependency] = [
+            .target(name: "\(name)"),
+            .project(target: "uTesting", path: .relativeToRoot("Projects/uTesting"))
+        ]
+        dependencies.forEach { (name) in
+            testsDependencies.append(.project(target: "\(name)Testing", path: .relativeToRoot("Projects/\(name)")))
+        }
+        
+        var targetDependencies: [TargetDependency] = dependencies.map({ .project(target: $0, path: .relativeToRoot("Projects/\($0)")) })
+        targetDependencies.append(contentsOf: sdks.map({.sdk(name: $0)}))
+        
+        var projectTargets: [Target] = []
+        if targets.contains(.framework) {
+            projectTargets.append(Target(name: name,
+                                         platform: .iOS,
+                                         product: .framework,
+                                         bundleId: "io.tuist.\(name)",
+                infoPlist: .default,
+                sources: ["Sources/**/*.swift"],
+                dependencies: targetDependencies,
+                settings: Settings(configurations: frameworkConfigurations)))
+        }
+        if targets.contains(.testing) {
+            projectTargets.append(Target(name: "\(name)Testing",
+                platform: .iOS,
+                product: .framework,
+                bundleId: "io.tuist.\(name)Testing",
+                infoPlist: .default,
+                sources: "Testing/**/*.swift",
+                dependencies: [.target(name: "\(name)")],
+                settings: Settings(configurations: frameworkConfigurations)))
+        }
+        if targets.contains(.tests) {
+            projectTargets.append(Target(name: "\(name)Tests",
+                platform: .iOS,
+                product: .unitTests,
+                bundleId: "io.tuist.\(name)Tests",
+                infoPlist: .default,
+                sources: "Tests/**/*.swift",
+                dependencies: testsDependencies,
+                settings: Settings(configurations: testsConfigurations)))
+        }
+        if targets.contains(.testing) {
+            projectTargets.append(Target(name: "\(name)Example",
+                platform: .iOS,
+                product: .app,
+                bundleId: "io.tuist.\(name)Examples",
+                infoPlist: .default,
+                sources: "Examples/**/*.swift",
+                dependencies: [.target(name: "\(name)")],
+                settings: Settings(configurations: appConfigurations)))
+        }
+        
         return Project(name: name,
                        organizationName: "Tuist",
                        settings: Settings(configurations: projectConfigurations),
-                       targets: [
-                        Target(name: name,
-                               platform: .iOS,
-                               product: .framework,
-                               bundleId: "io.tuist.\(name)",
-                            infoPlist: .default,
-                            sources: ["Sources/**/*.swift"],
-                            resources: ["Resources/**"],
-                            dependencies: dependencies,
-                            settings: Settings(configurations: frameworkConfigurations)),
-                        Target(name: "\(name)Testing",
-                            platform: .iOS,
-                            product: .framework,
-                            bundleId: "io.tuist.\(name)Testing",
-                            infoPlist: .default,
-                            sources: "Testing/**/*.swift",
-                            dependencies: [.target(name: "\(name)")],
-                            settings: Settings(configurations: frameworkConfigurations)),
-                        Target(name: "\(name)Tests",
-                            platform: .iOS,
-                            product: .unitTests,
-                            bundleId: "io.tuist.\(name)Tests",
-                            infoPlist: .default,
-                            sources: "Tests/**/*.swift",
-                            dependencies: [.target(name: "\(name)")],
-                            settings: Settings(configurations: testsConfigurations)),
-                        Target(name: "\(name)Example",
-                            platform: .iOS,
-                            product: .app,
-                            bundleId: "io.tuist.\(name)Examples",
-                            infoPlist: .default,
-                            sources: "Examples/**/*.swift",
-                            dependencies: [.target(name: "\(name)")],
-                            settings: Settings(configurations: appConfigurations))
-        ])
-    }
-
-    public static func project(name: String,
-                               product: Product,
-                               platform: Platform,
-                               dependencies: [TargetDependency] = [],
-                               infoPlist: [String: InfoPlist.Value] = [:]) -> Project {
-        return Project(name: name,
-                       targets: [
-                        Target(name: name,
-                                platform: platform,
-                                product: product,
-                                bundleId: "io.tuist.\(name)",
-                                infoPlist: .extendingDefault(with: infoPlist),
-                                sources: ["Sources/**"],
-                                resources: [],
-                                dependencies: dependencies),
-                        Target(name: "\(name)Tests",
-                                platform: platform,
-                                product: .unitTests,
-                                bundleId: "io.tuist.\(name)Tests",
-                                infoPlist: .default,
-                                sources: "Tests/**",
-                                dependencies: [
-                                    .target(name: "\(name)")
-                                ])
-                      ])
+                       targets: projectTargets)
     }
 
 }
