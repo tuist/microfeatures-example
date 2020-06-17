6eed8f20bb1a9a19ec86597553cc4db5d6f05270
Pedro Piñera <pedro@ppinera.es>
2020-06-17 11:01:53 +0200

    Set .xctest as a dependency

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -43,7 +43,8 @@ let project = Project(name: "App",
                                sources: ["Tests/**/*.swift"],
                                dependencies: [
                                 .target(name: "App"),
-                                // .project(target: "uTesting", path: .relativeToRoot("Projects/uTesting"))
+                                .project(target: "uTesting", path: .relativeToRoot("Projects/uTesting")),
+                                .xctest
                                 ],
                                settings: Settings(configurations: testsConfigurations)),
                         Target(name: "AppUITests",
@@ -54,7 +55,8 @@ let project = Project(name: "App",
                                sources: ["UITests/**/*.swift"],
                                dependencies: [
                                 .target(name: "App"),
-                                // .project(target: "uTesting", path: .relativeToRoot("Projects/uTesting"))
+                                .project(target: "uTesting", path: .relativeToRoot("Projects/uTesting")),
+                                .xctest
                                 ],
                                settings: Settings(configurations: testsConfigurations))
 ])
