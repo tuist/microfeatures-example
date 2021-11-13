3b050e8b01e10ebd6813117a7c3ec22bc738f074
Enrique Garcia <github@lemambo.com>
2021-11-13 22:31:17 +0100

    Fix swift 5 compilation error

--- Projects/uCore/Examples/Sources/AppDelegate.swift
+++ Projects/uCore/Examples/Sources/AppDelegate.swift
@@ -5,7 +5,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = UIViewController(nibName: nil, bundle: nil)
         window?.makeKeyAndVisible()
