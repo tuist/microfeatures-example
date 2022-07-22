e4eae2a93d4b4482be11a8d42c3ecd6d9c1c0f7e
Enrique Garcia <github@lemambo.com>
2022-07-22 19:12:13 +0200

    Fix class name

--- Projects/uUI/Tests/uUITests.swift
+++ Projects/uUI/Tests/uUITests.swift
@@ -2,7 +2,7 @@
 @testable import uUI
 import XCTest
 
-class uCoreTests: XCTestCase {
+class uUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
